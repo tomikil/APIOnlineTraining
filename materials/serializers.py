91c6f3c1c6fc2b802094f916182ecefdbb324a05
Tomilin Ivan <i.tomilin.91@yandex.ru>
2024-07-29 14:08:36 +0300

    create dz 25.2

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscription
+from materials.validators import LessonValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [LessonValidator(field='url_video')]
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -24,6 +26,21 @@ class CourseDetailSerializer(serializers.ModelSerializer):
     def get_count_lesson_in_course(self, course):
         return Lesson.objects.filter(course=course).count()
 
+    def get_subscription(self, course):
+        user = self.context.get('request').user
+        course = self.context.get('view').kwargs.get('pk')
+        subscription = Subscription.objects.filter(user=user, course=course)
+        if subscription.exists():
+            return True
+        else:
+            return False
+
     class Meta:
         model = Course
-        fields = ('title', 'description', 'preview', 'count_lesson_in_course', 'lesson')
+        fields = ('title', 'description', 'preview', 'count_lesson_in_course', 'lesson', 'subscription')
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = '__all__'
